c80fe7225a85eadfb3e95f5e82fed651ea1d906e
Aditya Baghure <adityabaghure24@gmail.com>
2026-06-03 19:30:02 +0530

    Update base.py

--- Compliance-OS--main/backend/models/base.py
+++ Compliance-OS--main/backend/models/base.py
@@ -16,6 +16,7 @@ class TimestampMixin:
         DateTime, default=datetime.datetime.utcnow, nullable=False
     )
     
+    
     updated_at = Column(
         DateTime,
         default=datetime.datetime.utcnow,
